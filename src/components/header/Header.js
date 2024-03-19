74ad9e9fed0fd56d90a1fd84f99f7d3553e25421
knikita <kallanikita116.com>
2024-03-19 23:56:09 +0530

    Updates

--- src/components/header/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function header() {
-  return <>
-        <div className='container'>
-            <h1 style= {{backgroundColor: 'purple', color:'white', textAlign:'center', margin:20}}>HAPPY BIRTHDAY BUDDUUUU..........</h1>
-        </div>
-  </>
-}
